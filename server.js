ebe1edc1d45e991245c87482301c5824f2c3361b
Saurav Kosti <kosti.saurav@gmail.com>
2024-12-01 00:05:50 +0530

    False

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(session({
         ttl: 24 * 60 * 60 // Session TTL in seconds
     }),
     cookie: {
-        secure: true,
+        secure: false,
         maxAge: 24 * 60 * 60 * 1000
     }
 }));
